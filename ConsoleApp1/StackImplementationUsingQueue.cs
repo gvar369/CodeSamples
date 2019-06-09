c9864bdb1b440d997968e5e731ef750613a26cc0
Himaja Reddy <himaja.125@gmail.com>
2019-06-08 23:55:14 -0500

    Added Code for stack implementation using queue

---
+++ ConsoleApp1/StackImplementationUsingQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSamples
+{
+    class StackImplementationUsingQueue
+    {
+        Queue<int> Q = new Queue<int>();
+        internal void Push(int element)
+        {
+            Q.Enqueue(element);
+        }
+        internal int Pop()  
+        {
+            Queue<int> Q1 = new Queue<int>();
+            while(Q.Count>1)  //Dequeue until last but 1 element
+            {
+                var temp = Q.Dequeue(); 
+                Q1.Enqueue(temp);
+                //Q1.Enqueue(Q.Dequeue());
+            }
+            var x = Q.Dequeue();//dequeue the last element
+            Q = Q1;
+            return x;
+        }
+
+    }
+}
